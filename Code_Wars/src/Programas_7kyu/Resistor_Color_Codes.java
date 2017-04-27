c43089936e8c4699aee7ad20df97739bcf384ed0
Yossy <Yossy@192.168.1.54>
2017-04-27 21:28:30 +0200

    Haciendo el Resistor Color de 7Kyu error: reached end of file while parsing

--- Code_Wars/src/Programas_7kyu/Resistor_Color_Codes.java
+++ Code_Wars/src/Programas_7kyu/Resistor_Color_Codes.java
@@ -9,6 +9,7 @@ package Programas_7kyu;
  *
  * @author Mauna Kea
  */
+caca
 public class Resistor_Color_Codes {
     public static String decodeResistorColors(String bands) {
         String solucion="";
@@ -20,14 +21,42 @@ public class Resistor_Color_Codes {
             solucion="10%";
             bands=bands.substring(0, bands.length()-7);
         }
-        else solucion="20%";
-       String[] colores=bands.split(" ");
-       String valores;
-       for(String valor:colores){
-           switch(valor){
-               case blue:           
-           }
-       }
-       return solucion;
+        else solucion = "20%";
+        String[] colores = bands.split(" ");
+        if (colores[2] != null)solucion = devuelveMultiplos(colores[2]) + solucion;
+        if (colores[1] != null)solucion = devuelveValores(colores[1]) + solucion;
+        solucion = devuelveValores(colores[0]) + solucion;
+        
+        return solucion;
+    }
+    public static String devuelveValores(String color){
+        switch(color){
+               case "black": return "0";
+               case "brown": return "1";
+               case "red": return "2";
+               case "orange": return "3";
+               case "yellow": return "4";
+               case "green": return "5";
+               case "blue": return "6";
+               case "violet": return "7";
+               case "gray": return "8";
+               case "white": return "9";
+        }
+        return null;
+    }
+    public static String devuelveMultiplos(String color){
+        switch(color){
+               case "black": return "";
+               case "brown": return "0";
+               case "red": return "00";
+               case "orange": return "K";
+               case "yellow": return "0K";
+               case "green": return "00K";
+               case "blue": return "M";
+               case "violet": return "0M";
+               case "gray": return "00M";
+               case "white": return "B";
+        }
+        return null;
     }
 }
