0b6cfe80ce69df64b55b47f9a9daa4d02f4123f9
Yossy <erosazul@gmail.com>
2017-05-04 14:40:10 +0200

    Twice Linear empezado

---
+++ Code_Wars/src/Programas_4Kyu/Twice_Linear.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Programas_4Kyu;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mauna Kea
+ */
+public class Twice_Linear {
+    public static int dblLinear (int n) {
+        ArrayList<Integer> sequencia
+    }
+}
