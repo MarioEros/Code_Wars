9287209937003e37aebd2e9a5c418f5af94c6016
Yossy <erosazul@gmail.com>
2017-05-05 14:06:53 +0200

    Diamante

---
+++ Code_Wars/src/Programas_6Kyu/Give_Me_A_Diamond.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Programas_6Kyu;
+
+/**
+ *
+ * @author Mauna Kea
+ */
+public class Give_Me_A_Diamond {
+    public static String print(int n) {
+        
+    }
+}
