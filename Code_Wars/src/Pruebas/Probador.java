a5cb02f9d0c237ed56896d9790027ef0861969bf
Yossy <Yossy@192.168.1.54>
2017-05-20 02:50:41 +0200

    Hunger games cajas abiertas?

--- Code_Wars/src/Pruebas/Probador.java
+++ Code_Wars/src/Pruebas/Probador.java
@@ -5,16 +5,10 @@
  */
 package Pruebas;
 
-import Programas_4Kyu.Catching_Car;
-import Programas_5Kyu.The_Road_Kill;
 import Programas_6Kyu.The_Hunger_Games;
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Scanner;
 /**
  *
  * @author Mauna Kea
@@ -24,17 +18,15 @@ public class Probador {
     public static void main(String[] args) throws IOException {
         //BufferedReader br = new BufferedReader(new FileReader("src/Pruebas/datos"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //Scanner in = new Scanner("datos");
         while (true) {
             String prueba = br.readLine();
-            //String prueba = in.nextLine();
             if (prueba.equalsIgnoreCase("fin"))break;
-            //String[] dividido = prueba.split(" ");
+//            String[] dividido = prueba.split(" ");
 //            double numdouble=Double.parseDouble(prueba);
 //            float numfloat=Float.parseFloat(prueba);
 //            byte numbyte=Byte.parseByte(prueba);
 //            short numshort=Short.parseShort(prueba);
-            int numint=Integer.parseInt(prueba);
+//            int numint=Integer.parseInt(prueba);
 //            long numlong=Long.parseLong(prueba);
 //            int[][] matrix = new int[][] { new int[] { 5, 3, 6, 1 },
 //                                 new int[] { 5, 8, 7, 4 },
@@ -44,7 +36,7 @@ public class Probador {
 //                    +matrix[1][matrix.length-1]+matrix[2][matrix.length-1]
 //                    +Arrays.toString(matrix[matrix.length-1])
 //                    +matrix[1][0]+matrix[2][0];
-            System.out.println(Catching_Car.isInteresting(numint, new int[]{12,23}));
+            System.out.println(The_Hunger_Games.hungryFoxes(prueba));
         }
     }
 }
