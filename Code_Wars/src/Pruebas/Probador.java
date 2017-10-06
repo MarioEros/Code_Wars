05f5025c5982d44bef45144223338d613035c049
Yossy <Yossy@192.168.1.54>
2017-10-06 20:59:24 +0200

    Lanzadados en el Probador

--- Code_Wars/src/Pruebas/Probador.java
+++ Code_Wars/src/Pruebas/Probador.java
@@ -9,6 +9,8 @@ import Programas_3Kyu.Calculator;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 /**
  *
  * @author Mauna Kea
@@ -21,8 +23,8 @@ public class Probador {
         while (true) {
             String prueba = br.readLine();
             if (prueba.equalsIgnoreCase("fin"))break;
-            String caso= "pepe\npaco";
-            String[] dividido = caso.split("\n");
+            //String caso= "pepe\npaco";
+            //String[] dividido = caso.split("\n");
 //            double uno=Double.parseDouble(dividido[0]);
 //            double dos=Double.parseDouble(dividido[1]);
 //            double numdouble=Double.parseDouble(prueba);
@@ -39,8 +41,24 @@ public class Probador {
 //                    +matrix[1][matrix.length-1]+matrix[2][matrix.length-1]
 //                    +Arrays.toString(matrix[matrix.length-1])
 //                    +matrix[1][0]+matrix[2][0];
-            System.out.println(caso);
-            System.out.println(dividido[0]+" "+dividido[1]);
+            //System.out.println(caso);
+            //System.out.println(dividido[0]+" "+dividido[1]);
+            Tirada();
         }
+    }
+    public static void Tirada(){
+        
+        ArrayList<Integer> resultadoDados=new ArrayList<>();
+        int dados = 12;
+        int caras = 10;
+        for (int i = 0; i<dados;i++){
+            resultadoDados.add(Dado(caras)+Dado(caras));
+        }
+        Collections.sort(resultadoDados);
+        for(int res:resultadoDados)System.out.print(res+",");
+        System.out.println();
+    }
+    public static int Dado(int x){
+        return (int)(Math.random() * x) + 1;
     }
 }
