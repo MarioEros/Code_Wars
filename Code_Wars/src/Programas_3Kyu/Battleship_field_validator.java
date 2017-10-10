e3670d36be538e469001eaa7d39af147981058f5
Eros <Eros@MaunaKea>
2017-10-10 14:03:19 +0200

    Battleship modificado

--- Code_Wars/src/Programas_3Kyu/Battleship_field_validator.java
+++ Code_Wars/src/Programas_3Kyu/Battleship_field_validator.java
@@ -17,33 +17,33 @@ public class Battleship_field_validator {
     static int submar1nes = 0;//10
     static boolean isNotShip=false;
 
-    private static void ship(int ux, int uy,int[][] ufield) {
+    private static void ship(int uy, int ux,int[][] ufield) {
         int eslora=0;
         boolean isShipEnd=false;
-        boolean isHorizontal=false;
+        boolean isVertical=false;
         boolean itsATrap=false;
-        for (int y = uy; y<10 && y < uy+4;) {
-            for (int x = ux; x<10 && x < ux+4;) {
+        for (int x = ux; x<10 && x < ux+4;) {
+            for (int y = uy; y<10 && y < uy+4;) {
                 
                     //Comprobamos si el barco es horizontal y si sigue
                 try{
-                    if(isHorizontal&&ufield[x+1][y]==0)isShipEnd=true;
-                    if(ufield[x+1][y]==1)isHorizontal=true;
-                    if(!isHorizontal&&ufield[x+1][y]==1)itsATrap=true;
+                    if(isVertical&&ufield[y+1][x]==0)isShipEnd=true;
+                    if(ufield[y+1][x]==1)isVertical=true;
+                    if(!isVertical&&ufield[y+1][x]==1)itsATrap=true;
                 }catch(Exception e){}
                 
                     //Comprobamos si el barco va hacia abajo
                 try{
-                    if(!isHorizontal && ufield[x][y+1]==0)isShipEnd=true;
-                    else if (isHorizontal && ufield[x][y+1]==1)itsATrap=true;
+                    if(!isVertical && ufield[y][x+1]==0)isShipEnd=true;
+                    else if (isVertical && ufield[y][x+1]==1)itsATrap=true;
                 }catch(Exception e){}
                 
                 //Comprobamos las diagonales por si fuera una trampa
                 try{
-                    if(ufield[x+1][y+1]==1)itsATrap=true;
+                    if(ufield[y+1][x+1]==1)itsATrap=true;
                 }catch(Exception e){}
                 try{
-                    if(ufield[x-1][y+1]==1)itsATrap=true;
+                    if(ufield[y-1][x+1]==1)itsATrap=true;
                 }catch(Exception e){}
                 
                 //Comprobamos si fué una trampa
@@ -52,8 +52,8 @@ public class Battleship_field_validator {
                 if(isShipEnd)break;
                 
                 //Sumamos la direccion del barco
-                if(isHorizontal)x++;
-                else y++;
+                if(isVertical)y++;
+                else x++;
             }
             //Comprobamos si fué una trampa
                 if(itsATrap)break;
@@ -77,9 +77,9 @@ public class Battleship_field_validator {
         de2troyers=0;
         submar1nes=0;
         isNotShip=false;
-        for (int y = 0; y < 10; y++) {
-            for (int x = 0; x < 10; x++) {
-                if (field[x][y] == 1)ship(x,y,field);
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                if (field[y][x] == 1)ship(y,x,field);
                 if (isNotShip)return false;
             }
         }
